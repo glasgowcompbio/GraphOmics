6e2abddb928b6dda5a683170153d93e866eef2e5
Joe Wandy <joe.wandy@glasgow.ac.uk>
2018-02-08 15:41:03 +0000

    Added a skeleton web app.

---
+++ web_omics/web_omics/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('linker.urls')),
+    path('linker/', include('linker.urls')),
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
