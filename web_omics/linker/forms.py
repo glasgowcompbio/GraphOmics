27189fb0d31b4206c760fb0b2575ebe4080f2f1b
Joe <joewandy@gmail.com>
2018-07-05 14:43:44 +0100

    Created better example data.

--- web_omics/linker/forms.py
+++ web_omics/linker/forms.py
@@ -19,9 +19,9 @@ def load_example_data(file_path):
     return example_data
 
 
-example_genes = load_example_data('../static/data/gene_data_small.csv')
+example_genes = load_example_data('../static/data/uploads/gene_data_example.csv')
 example_proteins = ""
-example_compounds = load_example_data('../static/data/compound_data.csv')
+example_compounds = load_example_data('../static/data/uploads/compound_data_example.csv')
 
 # example_genes = load_example_data('../static/data/gene_data.csv')
 # example_proteins = ""
