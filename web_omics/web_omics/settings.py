302eaac635203830b020afb6058cbe6fdccdf8ad
Rónán Daly <ronan.daly@glasgow.ac.uk>
2020-04-29 16:36:37 +0100

    Stopping suds logging at a low level

--- web_omics/web_omics/settings.py
+++ web_omics/web_omics/settings.py
@@ -54,6 +54,9 @@ class Common(Configuration):
             'matplotlib': {
                 'level': 'WARNING'
             },
+            'suds': {
+                'level': 'WARNING'
+            },
         },
     }
 
