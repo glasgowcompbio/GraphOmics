c556b129a4ae1c7dee59f98763d7c740a642464c
Rónán Daly <ronan.daly@glasgow.ac.uk>
2020-05-21 14:45:55 +0100

    Updating urls again.

--- web_omics/web_omics/urls.py
+++ web_omics/web_omics/urls.py
@@ -5,10 +5,14 @@ from django.urls import include, path
 import web_omics.views as views
 
 urlpatterns = [
-    path('', lambda request: redirect('app/', permanent=False)),
-    path('app/', views.ExperimentListView.as_view(), name='experiment_list_view'),
+    path('', views.ExperimentListView.as_view(), name='experiment_list_view'),
     path('linker/', include('linker.urls')),
     path('grappelli/', include('grappelli.urls')),  # grappelli URLS
     path('admin/', admin.site.urls),
     path('registration/', include('registration.urls')),
+]
+
+urlpatterns = [
+    path('', lambda request: redirect('app/', permanent=False)),
+    path('app/', include(urlpatterns))
 ]
