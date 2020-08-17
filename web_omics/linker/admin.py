74f8bac6ea86b4ae74f855b6eb0c47338d322a2a
Joe Wandy <joewandy@gmail.com>
2020-08-17 15:43:45 +0100

    Register AnalysisHistory with admin module

--- web_omics/linker/admin.py
+++ web_omics/linker/admin.py
@@ -4,6 +4,7 @@ from .models import *
 
 admin.site.register(Analysis)
 admin.site.register(AnalysisData)
+admin.site.register(AnalysisHistory)
 admin.site.register(AnalysisAnnotation)
 admin.site.register(AnalysisGroup)
 admin.site.register(Share)
